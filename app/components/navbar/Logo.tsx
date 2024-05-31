4cb3adfb8e5a2309ab7707c52d7fad927c3a14ea
Mohammed Haydar <mr.hamood277@gmail.com>
2024-05-31 20:06:38 +0100

    chore: Update logo size and add favicon

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -3,16 +3,17 @@ import Link from 'next/link'
 
 const Logo = () => {
   return (
-    <Link href='/'>
+    <Link href='/' className='flex gap-x-3 items-center'>
       <Image
-        className='hidden cursor-pointer md:block w-32 h-auto'
-        src='/images/logo.png'
+        className='hidden cursor-pointer md:block w-10 h-auto'
+        src='/images/logo-sm.png'
         width={0}
         height={0}
         sizes='100vw'
         alt='Logo'
         priority={true}
       />
+      <span className='font-semibold dark:text-gray-200'>StayMakan</span>
     </Link>
   )
 }
