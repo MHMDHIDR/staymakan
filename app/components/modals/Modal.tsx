d66faf87d1fb31636676000598aabeb55c3b7b84
Mohammed Haydar <mr.hamood277@gmail.com>
2024-10-02 20:30:02 +0100

    chore: Update .gitignore and Modal component
    - Add 'certificates' to .gitignore
    - Update Modal component to handle modal scrolling behavior

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -2,7 +2,6 @@
 
 import { useCallback, useEffect, useState } from 'react'
 import { IoMdClose } from 'react-icons/io'
-
 import Button from '../Button'
 
 interface ModalProps {
@@ -67,100 +66,52 @@ const Modal: React.FC<ModalProps> = ({
     return null
   }
 
+  // if modal is open then disable scrolling
+  if (showModal) {
+    document.body.style.overflow = 'hidden'
+  } else {
+    document.body.style.overflow = 'auto'
+  }
+
   return (
-    <>
-      <div
-        className='fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none 
-  focus:outline-none
- bg-neutral-800/70
-'
-      >
+    <div className='fixed inset-0 z-[10000] flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none bg-neutral-800/70'>
+      <div className='relative h-full mx-auto w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 lg:h-auto md:h-auto'>
         <div
-          className='relative h-full mx-auto 
-  w-full
- md:w-4/6
- lg:w-3/6
- xl:w-2/5
- my-6
- lg:h-auto
- md:h-auto
-'
+          className={` translate duration-300 h-full ${
+            showModal ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'
+          }`}
         >
-          {/*content*/}
-          <div
-            className={`
-            translate
-            duration-300
-            h-full
-            ${showModal ? 'translate-y-0' : 'translate-y-full'}
-            ${showModal ? 'opacity-100' : 'opacity-0'}
-          `}
-          >
-            <div
-              className='relative flex flex-col w-full bg-white dark:bg-neutral-900 border-0 rounded-lg shadow-lg outline-none 
-  translate
- h-full
- lg:h-auto
- md:h-auto
- focus:outline-none
-'
-            >
-              {/*header*/}
-              <div
-                className='
-                flex 
-                items-center 
-                p-6
-                rounded-t
-                justify-center
-                relative
-                border-b-[1px]
-                '
+          <div className='relative flex flex-col w-full bg-white dark:bg-neutral-900 border-0 rounded-lg shadow-lg outline-none translate h-full lg:h-auto md:h-auto focus:outline-none'>
+            <div className=' flex items-center p-6 rounded-t justify-center relative border-b-[1px]'>
+              <button
+                className='border-0  p-1 hover:opacity-70 transition absolute left-9'
+                onClick={handleClose}
               >
-                <button
-                  className='border-0 
-  p-1
- hover:opacity-70
- transition
- absolute
- left-9
-'
-                  onClick={handleClose}
-                >
-                  <IoMdClose size={18} />
-                </button>
-                <div className='text-lg font-semibold'>{title}</div>
-              </div>
-              {/*body*/}
-              <div className='relative flex-auto p-6'>{body}</div>
-              {/*footer*/}
-              <div className='flex flex-col p-6 gap-2'>
-                <div
-                  className='flex flex-row items-center 
-  gap-4 w-full
-'
-                >
-                  {secondaryAction && secondaryActionLabel && (
-                    <Button
-                      disabled={disabled}
-                      label={secondaryActionLabel}
-                      onClick={handleSecondaryAction}
-                      outline
-                    />
-                  )}
+                <IoMdClose size={18} />
+              </button>
+              <div className='text-lg font-semibold'>{title}</div>
+            </div>
+            {/*body*/}
+            <div className='relative flex-auto p-6'>{body}</div>
+            {/*footer*/}
+            <div className='flex flex-col p-6 gap-2'>
+              <div className='flex flex-row items-center gap-4 w-full'>
+                {secondaryAction && secondaryActionLabel && (
                   <Button
                     disabled={disabled}
-                    label={actionLabel}
-                    onClick={handleSubmit}
+                    label={secondaryActionLabel}
+                    onClick={handleSecondaryAction}
+                    outline
                   />
-                </div>
-                {footer}
+                )}
+                <Button disabled={disabled} label={actionLabel} onClick={handleSubmit} />
               </div>
+              {footer}
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
